f55695f9978bdfb1b5df5ce1e064632f5c991495
Alexander Stanislavsky <alexanderstanislavsky@Alexanders-MacBook-Air.local>
2019-06-05 16:06:27 +0300

    Router implementation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './App.css';
+import {Switch, Route} from 'react-router-dom'
+import Home from './components/Home';
+import Werk from './components/Werk';
+import Skills from './components/Skills';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Navigation/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/werk" component={Werk} />
+        <Route path="/skills" component={Skills} />
+      </Switch>
+    </div>
   );
 }
 
