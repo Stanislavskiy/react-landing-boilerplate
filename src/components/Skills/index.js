e2b422f771544c35ff077a0b0b13a0c127091e2d
Alexander Stanislavsky <alexanderstanislavsky@Alexanders-MacBook-Air.local>
2019-06-05 15:13:00 +0300

    Home, Werk, Skills components

---
+++ src/components/Skills/index.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+class Skills extends PureComponent {
+  render() {
+    return (
+      <div className="page">
+        <h1>Skills</h1>
+      </div>
+    )
+  }
+}
+
+export default Skills;
---
+++ src/components/Werk/index.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+class Werk extends PureComponent {
+  render() {
+    return (
+      <div className="page">
+        <h1>Werk</h1>
+      </div>
+    )
+  }
+}
+
+export default Werk;
