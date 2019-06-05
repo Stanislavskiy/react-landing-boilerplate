0f4a08dd93a7bd7f615d02e5d6f0b9e02f9c9a55
Alexander Stanislavsky <alexanderstanislavsky@Alexanders-MacBook-Air.local>
2019-06-05 16:06:13 +0300

    Navigation component

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,24 @@
+import React, { PureComponent } from 'react';
+import {Link} from 'react-router-dom';
+
+class Navigation extends PureComponent {
+  render() {
+    return (
+      <nav className="navigation">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/werk">Werk</Link>
+          </li>
+          <li>
+            <Link to="/skills">Skills</Link>
+          </li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Navigation;
