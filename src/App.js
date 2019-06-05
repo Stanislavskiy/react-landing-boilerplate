04bdcb0210f27eb61ab801fb84d238fe4f45c638
Alexander Stanislavsky <alexanderstanislavsky@Alexanders-MacBook-Air.local>
2019-06-05 16:21:37 +0300

    Page transitions animation

--- src/App.js
+++ src/App.js
@@ -5,16 +5,27 @@ import Home from './components/Home';
 import Werk from './components/Werk';
 import Skills from './components/Skills';
 import Navigation from './components/Navigation';
-
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
+ 
 function App() {
   return (
     <div className="App">
       <Navigation/>
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/werk" component={Werk} />
-        <Route path="/skills" component={Skills} />
-      </Switch>
+      <Route render={({location}) => (
+        <TransitionGroup>
+          <CSSTransition
+            key={location.key}
+            timeout={3000}
+            classNames="fade"
+          >
+            <Switch location={location}>
+              <Route exact path="/" component={Home}/>
+              <Route path="/werk" component={Werk} />
+              <Route path="/skills" component={Skills} />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
+      )} />
     </div>
   );
 }
